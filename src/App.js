b398d45fd698e6c8ccfefeaa034ad188f7feb757
Devloper-Pranav <pranavkumar2592@gmail.com>
2025-09-20 17:34:52 +0530

    Click Event with Function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import User from './User';
-
 
 function App() {
+
+  let data = "How are you i am fine.";
+
+  const demo = () => {
+    data = "Pranav Kumar";
+    // alert("Hello Everone");
+    alert(data);
+  }
   return (
     <>
     <div className="App">
-      <h1>Hello World !!</h1>
-      <User />
+      {/* <h1>Hello Everyone !!</h1> */}
+      <h1>{data}</h1>
+     {/* <button onClick={demo}>Click Me</button> */}
+     {/* <button onClick={()=> alert("Hello Everyone How are you?") }>Click Me</button> */}
+     <button onClick={()=> demo() }>Click Me</button>
     </div> 
     
     </>
